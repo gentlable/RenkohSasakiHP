0577ca5cc7d11632faa203ddaed7d33d3813bd42
gentlable <shingo.gt.1115@gmail.com>
2021-02-27 07:53:38 +0900

    テストページ本実装

--- app/Http/Controllers/RenkohSasakiController.php
+++ app/Http/Controllers/RenkohSasakiController.php
@@ -12,88 +12,28 @@ class RenkohSasakiController extends Controller
 {
     // トップページ
     public function index()
-    {   
-        // \Log::info(Image::where('position', 1)->where('web_display', 1)->first());
-        // 表示用画像配列作成 
+    {
         for ($i = 1; $i <= 20; $i++) {
-            if (Image::where('position', $i)->where('web_display', 1)->exists()) {
-                $image = Image::where('web_display', 1)->where('position', $i)->inRandomOrder()->first();
-                $file_name = $image['file_name'];
-                $web_images[] = "/images/$file_name";
-            } elseif (Image::where('position', $i)->where('web_display', 2)->exists()) {
-                $image = Image::where('web_display', 2)->where('position', $i)->inRandomOrder()->first();
-                $file_name = $image['file_name'];
-                $web_images[] = "/images/$file_name";
+
+            // 各段の画像の配列（ストーリー）
+            $file_paths_for_list = [];
+            // 一覧表示可のものを格納
+            if (Image::where('position', $i)->where('list_display', 2)->exists()) {
+                $images = Image::where('position', $i)->where('list_display', 2)->orderBy('file_name', 'asc')->get();
+                foreach ($images as $image) {
+                    $file_name = $image['file_name'];
+                    $file_path_for_list = "/images/$file_name";
+                    $file_paths_for_list[] = $file_path_for_list;
+                }
             } else {
                 continue;
             }
-        }
-        \Log::info($web_images);
-        // 一段目の画像をセット
-        $first_web_image = $web_images[0];
-        // 一段目の画像を配列から削除
-        unset($web_images[0]);
-        \Log::info($web_images);
-        // 一段目の画像を配列から削除
-        $web_images = array_values($web_images);
-
-        // ポップアップ用画像配列作成
-        for ($i = 1; $i <= 20; $i++) {
-            // 一覧表示するものがなかければ、スキップ
-            if (Image::where('position', $i)->where('list_display', 1)->exists() === false &&
-                Image::where('position', $i)->where('list_display', 2)->exists() === false) {
-                    continue;
-            }
-
-            // 一覧表示優先の物を追加
-            $file_paths_for_list = [];
-            if (Image::where('position', $i)->where('list_display', 1)->exists()) {
-                $images = Image::where('position', $i)->where('list_display', 1)->inRandomOrder()->get();
-                $file_paths_for_list = [];
-                foreach ($images as $image) {
-                    $file_name = $image['file_name'];
-                    $file_path_for_list = "/images/$file_name";
-                    $file_paths_for_list[] = $file_path_for_list;
-                }
-            }
-
-            // 一覧表示可の物を追加
-            if (Image::where('position', $i)->where('list_display', 2)->exists()) {
-                $images = Image::where('position', $i)->where('list_display', 2)->inRandomOrder()->get();
-                foreach ($images as $image) {
-                    $file_name = $image['file_name'];
-                    $file_path_for_list = "/images/$file_name";
-                    $file_paths_for_list[] = $file_path_for_list;
-                }
-            }
-
-            // モーダル表示用配列にセット
             $modal_images[] = $file_paths_for_list;
         }
 
-        // 一段目のポップアップ画像をセット
-        $first_modal_images = $modal_images[0];
-        // 一段目のポップアップ画像を配列から削除
-        unset($modal_images[0]);
-
-        $top_first_modal_image= $first_modal_images[0];
-        unset($first_modal_images[0]);
-
-        
-        foreach ($modal_images as $i => $modal_image) {
-            $top_modal_images[] = $modal_image[0];
-            unset($modal_images[$i][0]);
-        }
         $modal_images = array_values($modal_images);
 
         return view("renkohsasaki.index", [
-            'first_web_image' => $first_web_image,
-            'web_images' => $web_images,
-
-            'top_first_modal_image' => $top_first_modal_image,
-            'first_modal_images' => $first_modal_images,
-
-            'top_modal_images' => $top_modal_images,
             'modal_images' => $modal_images,
             ]);
     }
--- resources/views/renkohsasaki/index.blade.php
+++ resources/views/renkohsasaki/index.blade.php
@@ -3,115 +3,38 @@
 @section('content')
 <!-- メイン -->
 <main>
-    <div class="container text-center">
-        <!-- 一枚目の写真 sp -->
-        <div class="d-block d-md-none">
-            @if (count($first_modal_images) === 0)
-            <div style="margin-top:100px; margin-bottom:250px">
-                <img src="{{ asset($first_web_image) }}" class="img-responsive" style="width:100%;">
+    @foreach($modal_images as $i => $modal_image)
+    <div class="section">
+        <div id="carouselExampleFade_{{ $i }}" class="carousel slide carousel-fade" data-interval="false">
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img src="{{ asset($modal_image[0]) }}">
+                </div>
+                @if (count($modal_images[$i]) > 2)
+                @for($j = 1; $j <= count($modal_images[$i]) - 1; $j++)
+                <div class="carousel-item">
+                    <img src="{{ asset($modal_image[$j]) }}">
+                </div>
+                @endfor
+                @endif
             </div>
-            @else
-            <div style="margin-top:100px; margin-bottom:250px">
-                <a data-toggle="modal" data-target="#FirstImage"><img src="{{ asset($first_web_image) }}" class="img-responsive" style="width:100%;"></a>
-            </div>
+            @if (count($modal_images[$i]) > 2)
+            <a class="carousel-control-prev" href="#carouselExampleFade_{{ $i }}" role="button" data-slide="prev">
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleFade_{{ $i }}" role="button" data-slide="next">
+            </a>
+            <ol class="carousel-indicators">
+                <li data-target="#carouselExampleFade_{{ $i }}" data-slide-to="0" class="active"></li>
+                @for ($j = 1; $j <= count($modal_images[$i]) - 1; $j++)
+                <li data-target="#carouselExampleFade_{{ $i }}" data-slide-to="{{ $j }}"></li>
+                @endfor
+            </ol>
             @endif
         </div>
-        <!-- 一枚目の写真 pc -->
-        <div class="d-none d-md-block">
-            @if (count($first_modal_images) === 0)
-            <div style="margin-top:72px; margin-bottom:30px;">
-                <img src="{{ asset($first_web_image) }}" class="img-responsive" style="max-height:700px; max-width:100%;">
-            </div>
-            @else
-            <div style="margin-top:72px; margin-bottom:30px;">
-                <a data-toggle="modal" data-target="#FirstImage"><img src="{{ asset($first_web_image) }}" class="img-responsive" style="max-height:700px; max-width:100%"></a>
-            </div>
-            @endif
-        </div>
-        @foreach ($web_images as $i => $web_image)
-        <!-- 二枚目以降の写真 sp -->
-        <div class="d-block d-md-none">
-            @if (count($modal_images[$i]) === 0)
-            <div style="margin-top:30px; margin-bottom:250px;">
-                <img src="{{ asset($web_image) }}" class="img-responsive" style="width:100%;">
-            </div>
-            @else
-            <div style="margin-top:30px; margin-bottom:250px;">
-                <a data-toggle="modal" data-target="#modal_{{ $i }}"><img src="{{ asset($web_image) }}" class="img-responsive" style="width:100%;"></a>
-            </div>
-            @endif
-        </div>
-        <!-- 二枚目以降の写真 pc -->
-        <div class="d-none d-md-block">
-            @if (count($modal_images[$i]) === 0)
-            <div style="margin-top:300px; margin-bottom:200px">
-                <img src="{{ asset($web_image) }}" class="img-responsive" style="max-height:700px; max-width:100%">
-            </div>
-            @else
-            <div style="margin-top:300px; margin-bottom:200px">
-                <a data-toggle="modal" data-target="#modal_{{ $i }}"><img src="{{ asset($web_image) }}" class="img-responsive" style="max-height:700px; max-width:100%"></a>
-            </div>
-            @endif
-        </div>
-        @endforeach
     </div>
+    @endforeach
 </main>
 
-<!-- モーダル 最初の写真 -->
-<div class="modal fade" id="FirstImage" tabindex="-1" role="dialog" aria-labelledby="Modal" aria-hidden="true" style="padding-top:60px;">
-    <div class="modal-dialog modal-lg" role="document" style="max-width: 600px;">
-        <div class="modal-content">
-            <button type="button" class="close" data-dismiss="modal" aria-label="閉じる"></button>
-            <div id="carouselExampleFade" class="carousel slide carousel-fade" data-ride="carousel">
-                <div class="carousel-inner">
-                    <div class="carousel-item active">
-                        <img class="d-block w-100" src="{{ asset($top_first_modal_image) }}">
-                    </div>
-                    @foreach($first_modal_images as $first_modal_image)
-                    <div class="carousel-item">
-                        <img class="d-block w-100" src="{{ asset($first_modal_image) }}">
-                    </div>
-                    @endforeach
-                </div>
-                <a class="carousel-control-prev" href="#carouselExampleFade" role="button" data-slide="prev">
-                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                </a>
-                <a class="carousel-control-next" href="#carouselExampleFade" role="button" data-slide="next">
-                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                </a>
-            </div>
-        </div>
-    </div>
-</div>
-
-<!-- モーダル ２番目以降の写真 -->
-@foreach($top_modal_images as $i => $top_modal_image)
-<div class="modal fade" id="modal_{{ $i }}"  tabindex="-1" role="dialog" aria-labelledby="Modal" aria-hidden="true" style="padding-top:60px;">
-    <div class="modal-dialog modal-lg" role="document" style="max-width: 600px;">
-        <div class="modal-content">
-            <button type="button" class="close" data-dismiss="modal" aria-label="閉じる"></button>
-            <div id="carouselExampleFade_{{ $i }}" class="carousel slide carousel-fade" data-ride="carousel">
-                <div class="carousel-inner">
-                    <div class="carousel-item active">
-                        <img class="d-block w-100" src="{{ asset($top_modal_image) }}">
-                    </div>
-                    @foreach($modal_images[$i] as $modal_image)
-                    <div class="carousel-item">
-                        <img class="d-block w-100" src="{{ asset($modal_image) }}">
-                    </div>
-                    @endforeach
-                </div>
-                <a class="carousel-control-prev" href="#carouselExampleFade_{{ $i }}" role="button" data-slide="prev">
-                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                </a>
-                <a class="carousel-control-next" href="#carouselExampleFade_{{ $i }}" role="button" data-slide="next">
-                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                </a>
-            </div>
-        </div>
-    </div>
-</div>
-@endforeach
 
 <!-- メール送信用モーダルの設定 -->
 <div class="modal fade" id="Modal" tabindex="-1" role="dialog" aria-labelledby="Modal" aria-hidden="true">
@@ -184,4 +107,36 @@
     </div>
   </div>
 </div>
+
+<style>
+.section {
+    max-height: 100vh;
+}
+.carousel {
+    margin: auto;
+    margin-top: 10vh;
+    margin-bottom: 15vh;
+    /* 0.79 縦横の比率 */
+    max-width: calc(80vh * 0.79);
+}
+.carousel a {
+    width: 50%;
+}
+.carousel-item {
+    text-align: center;
+}
+.carousel-item img {
+    max-height: 80vh;
+    max-width: 100%;
+    object-fit: cover;
+}
+.carousel-indicators {
+    bottom: -45px;
+}
+.carousel-indicators li {
+    background-color: #000;
+    width: 15px;
+}
+</style>
+
 @endsection
